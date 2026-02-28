c3b575cbe4f13d7063e357ce046d609cf0b2d558
wigsdev <karlwgs1989@gmail.com>
2026-02-28 00:51:12 -0500

    feat(db): configurar application.properties y migracion Flyway V1 tabla topicos (HU 02, HU 03)

---
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE topicos (
+
+    id          BIGINT          NOT NULL AUTO_INCREMENT,
+    titulo      VARCHAR(200)    NOT NULL,
+    mensaje     TEXT            NOT NULL,
+    fecha_creacion DATETIME     NOT NULL DEFAULT NOW(),
+    status      VARCHAR(50)     NOT NULL DEFAULT 'ABIERTO',
+    autor       VARCHAR(100)    NOT NULL,
+    curso       VARCHAR(100)    NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_topicos_titulo_mensaje (titulo, mensaje)
+);
