bff6f1240386d139fbbdd4573fb06dee30501ca6
wigsdev <karlwgs1989@gmail.com>
2026-02-28 01:25:48 -0500

    fix(db): cambiar mensaje de TEXT a VARCHAR(1000) para soporte de UNIQUE KEY en MySQL

--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -1,13 +1,13 @@
 CREATE TABLE topicos (
 
-    id          BIGINT          NOT NULL AUTO_INCREMENT,
-    titulo      VARCHAR(200)    NOT NULL,
-    mensaje     TEXT            NOT NULL,
-    fecha_creacion DATETIME     NOT NULL DEFAULT NOW(),
-    status      VARCHAR(50)     NOT NULL DEFAULT 'ABIERTO',
-    autor       VARCHAR(100)    NOT NULL,
-    curso       VARCHAR(100)    NOT NULL,
+    id             BIGINT          NOT NULL AUTO_INCREMENT,
+    titulo         VARCHAR(200)    NOT NULL,
+    mensaje        VARCHAR(1000)   NOT NULL,
+    fecha_creacion DATETIME        NOT NULL DEFAULT NOW(),
+    status         VARCHAR(50)     NOT NULL DEFAULT 'ABIERTO',
+    autor          VARCHAR(100)    NOT NULL,
+    curso          VARCHAR(100)    NOT NULL,
 
     PRIMARY KEY (id),
-    UNIQUE KEY uq_topicos_titulo_mensaje (titulo, mensaje)
+    UNIQUE KEY uq_topicos_titulo_mensaje (titulo, mensaje(255))
 );
