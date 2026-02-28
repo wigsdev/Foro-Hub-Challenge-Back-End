4bb638aaccc344d850e5b7c5c1612c19ef65b64b
wigsdev <karlwgs1989@gmail.com>
2026-02-28 10:51:31 -0500

    feat(api): implementar endpoints GET /topicos (listado con paginación) y GET /topicos/{id} (detalle) (HU 05 y HU 06)

--- src/main/java/com/aluralatam/forohub/controller/TopicoController.java
+++ src/main/java/com/aluralatam/forohub/controller/TopicoController.java
@@ -6,11 +6,15 @@ import com.aluralatam.forohub.domain.topico.Topico;
 import com.aluralatam.forohub.domain.topico.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos")
@@ -24,7 +28,6 @@ public class TopicoController {
             @RequestBody @Valid DatosRegistroTopico datos,
             UriComponentsBuilder uriBuilder) {
 
-        // Regla de negocio: no permitir duplicados (mismo titulo y mensaje)
         if (topicoRepository.existsByTituloAndMensaje(datos.titulo(), datos.mensaje())) {
             return ResponseEntity.badRequest().build();
         }
@@ -34,5 +37,24 @@ public class TopicoController {
 
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DatosRespuestaTopico(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DatosRespuestaTopico>> listadoTopicos(
+            @PageableDefault(size = 10, sort = "fechaCreacion") Pageable paginacion) {
+        // En un Listado, usamos Page para devolver subconjuntos en formato JSON
+        // ordenado y fácil de leer
+        return ResponseEntity.ok(topicoRepository.findAll(paginacion).map(DatosRespuestaTopico::new));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DatosRespuestaTopico> detallarTopico(@PathVariable Long id) {
+        Optional<Topico> topicoEncontrado = topicoRepository.findById(id);
+
+        if (topicoEncontrado.isPresent()) {
+            return ResponseEntity.ok(new DatosRespuestaTopico(topicoEncontrado.get()));
+        } else {
+            return ResponseEntity.notFound().build(); // HTTP 404
+        }
     }
 }
