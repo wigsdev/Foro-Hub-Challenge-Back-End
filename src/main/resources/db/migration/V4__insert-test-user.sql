2b0a555455bc31173d937dcc96ae0216d3e68f57
wigsdev <karlwgs1989@gmail.com>
2026-02-28 21:30:18 -0500

    chore(QA): inyectar usuario de pruebas E2E mediante flyway (HU26)

---
+++ src/main/resources/db/migration/V4__insert-test-user.sql
@@ -0,0 +1,3 @@
+-- Insertar usuario de prueba nativo para evaluación QA E2E
+-- Password en texto plano es: 123456
+INSERT INTO usuarios (login, clave) VALUES ('admin@aluracursos.com', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.');
